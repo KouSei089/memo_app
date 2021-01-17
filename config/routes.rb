b4b7e426fdccb659480d45931e4320353e6d9e2f
KouSei089 <qq00891829@icloud.com>
2021-01-17 15:09:50 +0900

    memosControllerCreate

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'top#top'
 
-  namespace :api do
-   #API用のルーティングはここに書く
+  namespace :api, format: 'json' do
+    resources :memos, only: [:index]
   end
 
   get '*path', to: 'top#top', format: false   #getのリクエストをまとめる
